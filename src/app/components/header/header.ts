998e0523b13391ed3a20653b5745cd9623829a73
7atoom <ugs.1331@ci.suez.edu.eg>
2026-01-22 15:48:51 +0200

    add header, footer, error, and product-related components with initial templates

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './header.html',
+  styles: ``,
+})
+export class Header {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+}
